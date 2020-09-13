9f2556945cd5dd1fdb8c5ca8686e0d4471f20e5d
Karol Ryska <karolryska@gmail.com>
2020-09-13 20:16:43 +0200

    Add counter feature

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,42 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+const wrapper = document.querySelector(".wrapper");
+wrapper.style.height = `${window.innerHeight}px`;
+
+const counter = document.querySelector(".counter__number");
+
+const addButton = document.querySelector(".button--add");
+const removeButton = document.querySelector(".button--remove");
+
+const today = new Date().toISOString().slice(0, 10);
+
+let counterNumber;
+
+const counterReload = () => {
+    counter.textContent = counterNumber;
+}
+
+if (JSON.parse(localStorage.getItem(today))) {
+    counterNumber = JSON.parse(localStorage.getItem(today));
+    counterReload();
+} else {
+    counterNumber = 0;
+    counterReload();
+}
+
+const storageReload = () => {
+    localStorage.setItem(today, JSON.stringify(counterNumber));
+}
+
+addButton.addEventListener("click", () => {
+    counterNumber++;
+    counterReload();
+    storageReload();
+})
+
+removeButton.addEventListener("click", () => {
+    if (counterNumber >= 1) counterNumber--;
+    counterReload();
+    storageReload();
+})
