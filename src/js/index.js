91dd981d5036833ca1a14f71b84c44fdabfad9c4
Karol Ryska <karolryska@gmail.com>
2020-09-16 23:31:52 +0200

    Fix getting keys from localStorage

--- src/js/index.js
+++ src/js/index.js
@@ -9,6 +9,9 @@ import '../scss/main.scss';
 const wrapper = document.querySelector(".wrapper");
 wrapper.style.height = `${window.innerHeight}px`;
 
+// const items = JSON.parse(JSON.stringify(localStorage));
+// console.log(items);
+
 const counter = document.querySelector(".counter__number");
 const history = document.querySelector(".content__history");
 const historyList = document.querySelector(".history__list");
@@ -18,26 +21,32 @@ const removeButton = document.querySelector(".button--remove");
 const historyButton = document.querySelector(".button-history");
 
 const today = new Date().toISOString().slice(0, 10);
-let historyArray = [];
-historyArray.push(today);
 
+let historyArray = [];
 let counterNumber;
 
+if (!JSON.parse(localStorage.getItem('history'))) {
+    localStorage.setItem('history', JSON.stringify(historyArray));
+}
 
 const counterReload = () => {
     counter.textContent = counterNumber;
 }
 
+const storageReload = () => {
+    localStorage.setItem(today, JSON.stringify(counterNumber));
+}
+
 if (JSON.parse(localStorage.getItem(today))) {
+    historyArray = JSON.parse(localStorage.getItem('history'));
     counterNumber = JSON.parse(localStorage.getItem(today));
     counterReload();
 } else {
+    historyArray = JSON.parse(localStorage.getItem('history'));
+    historyArray.push(today);
+    localStorage.setItem('history', JSON.stringify(historyArray));
     counterNumber = 0;
     counterReload();
-}
-
-const storageReload = () => {
-    localStorage.setItem(today, JSON.stringify(counterNumber));
 }
 
 addButton.addEventListener("click", () => {
